3a6f0e32795303996f1627e6c109c6951596d37f
timizdepot <timizdepot@gmail.com>
2022-02-12 16:38:35 -0500

    setup infrastructure

--- jenkins.sh
+++ jenkins.sh
@@ -7,4 +7,6 @@ apt install openjdk-11-jdk wget -y
 wget https://updates.jenkins-ci.org/download/war/2.334/jenkins.war
 cd /opt/
 wget https://dlcdn.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.tar.gz
+tar -xvzf apache-maven-3.8.4-bin.tar.gz
+rm -f apache-maven-3.8.4-bin.tar.gz
 nohup java -jar jenkins.war &
--- tomcat.sh
+++ tomcat.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 yum update -y
 yum install java-11-openjdk.x86_64 wget -y
+cd /opt/
 wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.16/bin/apache-tomcat-10.0.16.tar.gz
+tar -xvzf apache-tomcat-10.0.16.tar.gz
+rm -f apache-tomcat-10.0.16.tar.gz
+cd apache-tomcat-10.0.16/bin/
+chmod +x startup.sh shutdown.sh
+ln -s /opt/apache-tomcat-10.0.16/bin/startup.sh /usr/bin/tomcatup
+ln -s /opt/apache-tomcat-10.0.16/bin/shutdown.sh /usr/bin/tomcatdown
