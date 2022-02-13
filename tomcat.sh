fb509b72f5a840c58651012f92dd827f79096acf
timizdepot <timizdepot@gmail.com>
2022-02-12 20:25:55 -0500

    update tomcat userdata

--- tomcat.sh
+++ tomcat.sh
@@ -2,10 +2,10 @@
 yum update -y
 yum install java-11-openjdk.x86_64 wget -y
 cd /opt/
-wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.16/bin/apache-tomcat-10.0.16.tar.gz
-tar -xvzf apache-tomcat-10.0.16.tar.gz
-rm -f apache-tomcat-10.0.16.tar.gz
-cd apache-tomcat-10.0.16/bin/
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.58/bin/apache-tomcat-9.0.58.tar.gz
+tar -xvzf apache-tomcat-9.0.58.tar.gz
+rm -f apache-tomcat-9.0.58.tar.gz
+cd apache-tomcat-9.0.58/bin/
 chmod +x startup.sh shutdown.sh
-ln -s /opt/apache-tomcat-10.0.16/bin/startup.sh /usr/bin/tomcatup
-ln -s /opt/apache-tomcat-10.0.16/bin/shutdown.sh /usr/bin/tomcatdown
+ln -s /opt/apache-tomcat-9.0.58/bin/startup.sh /usr/bin/tomcatup
+ln -s /opt/apache-tomcat-9.0.58/bin/shutdown.sh /usr/bin/tomcatdown
